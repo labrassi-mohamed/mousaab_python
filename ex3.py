d50a8f3ad091fce856b2f93712e06a2cbb3b17c5
labrassimousaab <labrassi.mousaab@gmail.com>
2025-06-11 17:30:26 +0000

    ex3

---
+++ ex3.py
@@ -0,0 +1,9 @@
+def hours_to_minute(x):
+    minute = x * 60
+    return minute
+
+user_input =input("enter the hours :")
+num = int(user_input)
+
+the_minute_is = hours_to_minute(num) 
+print ("enter the hour" , the_minute_is)
