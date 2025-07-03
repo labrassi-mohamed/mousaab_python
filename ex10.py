7d2e0c70ac147420c76ebb24ed32fba3d6a6dc48
labrassimousaab <labrassi.mousaab@gmail.com>
2025-07-03 18:13:30 +0200

    ex10.py

---
+++ ex10.py
@@ -0,0 +1,7 @@
+def count_word (sentence):
+    word_list = sentence.split()
+    print(word_list)
+    return len(word_list)
+user_input =input ("pls enter ure sentence here :")
+num_word =count_word(user_input)
+print ( f"the number of words in this sentence is : {num_word}")
