e5a724bc7a348202871dee536418341504c88532
labrassimousaab <labrassi.mousaab@gmail.com>
2025-07-16 17:06:44 +0000

    class.py

---
+++ OOP/class.py
@@ -0,0 +1,14 @@
+class humain :
+    #attributs
+    name = ""
+    gender = ""
+    age = 0
+    city = marrakech
+#instanciation
+hum1 = humain()
+hum1.gender= male
+hum1.name= hmimid
+hum1.age= 10000
+#affichage 
+print(hum1.name)
+print(humain.city)
