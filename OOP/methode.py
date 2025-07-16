a03fb706223c84636b7234a75d6e26dbb9f97011
labrassimousaab <labrassi.mousaab@gmail.com>
2025-07-16 17:14:21 +0000

    methodes.py

---
+++ OOP/methode.py
@@ -0,0 +1,14 @@
+class humain :
+    #attributs
+    name = ""
+    gender = ""
+    age = 0
+    
+    #methodes
+    def aficher (self):
+        print("the name is "+ self.name)
+#instanciation
+hum1 = humain()
+hum1.name= hmimid
+hum1.aficher()
+
