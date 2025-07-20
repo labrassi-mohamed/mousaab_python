faecdef04af9fe642e8f837fd1f43cf92868e0d3
labrassimousaab <labrassi.mousaab@gmail.com>
2025-07-20 21:18:17 +0200

    Constructor.py

---
+++ OOP/ Constructor.py
@@ -0,0 +1,11 @@
+class Utudiant :
+    def __unit__(self,name,myAge,myNote):
+        self.nom =name
+        self.age =myAge
+        self.note = myNote
+    def ajouterNote (self , points):
+        print(self.note + points)
+
+etd1 = Utudiant("hesan" , 16 ,14)
+print(etd1.age)
+etd1.ajouterNote(1)
