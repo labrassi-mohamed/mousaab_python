abc6e49760372d11a68731fa608b57db5992a3ad
labrassimousaab <labrassi.mousaab@gmail.com>
2025-07-24 19:09:15 +0200

    Héritage2.py

---
+++ OOP/ Héritage2.py
@@ -0,0 +1,24 @@
+class Person ():
+    def __init__(self , nm , ag):
+        self.name = nm
+        self.age = ag
+    def afficher(self):
+        print("Le nom est :" , self.name)
+
+class Etudiant (Person):
+    def __init__(self , nm,ag,nt):
+        Person.__init__(self , name,age )
+        setf.note = nt
+    def afficher(self):
+        super().aficher()
+class Professeur (Person):
+    def __init__(self , nm,ag,sal):
+        Person.__init__(self , name,age )
+        setf.salaire = sal
+    def afficher(self):
+        super().aficher()
+
+etd1 = Etudiant("Asmae",16 ,14)
+print(etd1.name)
+prof1 = Professeur("Mohemed" , 25 , 50000)
+print(prof1.name)
